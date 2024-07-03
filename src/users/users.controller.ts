246d9b9943aa43e7624449a1d459954b323e5627
Tomasz Steć <tomeczekstecc@gmail.com>
2024-07-03 23:19:22 +0200

    added routes

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,49 @@
-import { Controller } from '@nestjs/common';
+import {Body, Controller, Delete, Get, Param, Patch, Post, Query} from '@nestjs/common';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+    /*
+    /users (GET)
+    /users/:id (GET)
+    /users (POST)
+    /users/:id (PATCH)
+    /users/:id (DELETE)
+    */
+
+    @Get() // /users (GET)
+    findAll(@Query('role') role?: 'INTERN' | 'USER' | 'ADMIN') {
+        if (role) {
+            return `This action returns all ${role}s`;
+        }
+        return 'This action returns all users';
+    }
+
+    @Get('interns') // /users/interns (GET)
+    findInterns() {
+        return 'This action returns all interns';
+    }
+
+
+    @Get(':id') // /users/:id (GET)
+    findOne(@Param('id') id: string) {
+        return {id};
+    }
+
+    @Post() // /users (POST)
+    create(@Body() user: {}) {
+        return 'This action adds a new user';
+    }
+
+
+    @Patch(':id') // /users/:id (PATCH)
+    update(@Param('id') id: string, @Body() userUpdate: {}) {
+        return {id, ...userUpdate};
+    }
+
+
+    @Delete(':id') // /users/:id (DELETE)
+    delete(@Param('id') id: string) {
+        return 'This action removes a user';
+    }
+
+}
