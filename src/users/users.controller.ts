ffb4d13ffea2a8484de81fa2a860811301ed6ff0
stect <stect@slaskie.pl>
2024-07-04 16:07:07 +0200

    users

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,8 +1,17 @@
-import {Body, Controller, Delete, Get, Param, Patch, Post, Query} from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+  Query,
+} from '@nestjs/common';
 
 @Controller('users')
 export class UsersController {
-    /*
+  /*
     /users (GET)
     /users/:id (GET)
     /users (POST)
@@ -10,40 +19,36 @@ export class UsersController {
     /users/:id (DELETE)
     */
 
-    @Get() // /users (GET)
-    findAll(@Query('role') role?: 'INTERN' | 'USER' | 'ADMIN') {
-        if (role) {
-            return `This action returns all ${role}s`;
-        }
-        return 'This action returns all users';
+  @Get() // /users (GET)
+  findAll(@Query('role') role?: 'INTERN' | 'USER' | 'ADMIN') {
+    if (role) {
+      return `This action returns all ${role}s`;
     }
+    return 'This action returns all users';
+  }
 
-    @Get('interns') // /users/interns (GET)
-    findInterns() {
-        return 'This action returns all interns';
-    }
+  @Get('interns') // /users/interns (GET)
+  findInterns() {
+    return 'This action returns all interns';
+  }
 
+  @Get(':id') // /users/:id (GET)
+  findOne(@Param('id') id: string) {
+    return { id };
+  }
 
-    @Get(':id') // /users/:id (GET)
-    findOne(@Param('id') id: string) {
-        return {id};
-    }
+  @Post() // /users (POST)
+  create(@Body() user: NonNullable<unknown>) {
+    return user;
+  }
 
-    @Post() // /users (POST)
-    create(@Body() user: {}) {
-        return 'This action adds a new user';
-    }
+  @Patch(':id') // /users/:id (PATCH)
+  update(@Param('id') id: string, @Body() userUpdate: {}) {
+    return { id, ...userUpdate };
+  }
 
-
-    @Patch(':id') // /users/:id (PATCH)
-    update(@Param('id') id: string, @Body() userUpdate: {}) {
-        return {id, ...userUpdate};
-    }
-
-
-    @Delete(':id') // /users/:id (DELETE)
-    delete(@Param('id') id: string) {
-        return 'This action removes a user';
-    }
-
+  @Delete(':id') // /users/:id (DELETE)
+  delete(@Param('id') id: string) {
+    return `This action removes a #${id} user`;
+  }
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,16 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  private users = [
+    { id: 1, name: 'John', role: 'ADMIN', email: 'john@mail.com' },
+    { id: 2, name: 'Doe', role: 'USER', email: 'doe@mail.com' },
+    { id: 3, name: 'Smith', role: 'INTERN', email: 'smith@mail.com' },
+    { id: 4, name: 'Jane', role: 'USER', email: 'jane@mail.com' },
+    { id: 5, name: 'Alice', role: 'ADMIN', email: 'alice@mail.com' },
+  ];
+
+  findAll(role?: 'INTERN' | 'USER' | 'ADMIN') {
+    return this.users;
+  }
+}
